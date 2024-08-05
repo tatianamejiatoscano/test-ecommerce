a5e44e030457f69244b2278e917231394b0914f6
Tatiana Mejia <tatiana.mejia.toscano@gmail.com>
2024-08-05 13:54:58 +0200

    PATCH - Initial project configuration

---
+++ src/main/java/com/tmt/ecommerce/TestEcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.tmt.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEcommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestEcommerceApplication.class, args);
+  }
+}
---
+++ src/test/java/com/tmt/ecommerce/TestEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmt.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
