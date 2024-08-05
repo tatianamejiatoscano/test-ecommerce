df4ea19088ad944828ac02db6e8bdd7d9dd21c83
Tatiana Mejia <tatiana.mejia.toscano@gmail.com>
2024-08-05 14:13:32 +0200

    PATCH - Add PriceService implementation

---
+++ src/main/java/com/tmt/ecommerce/application/services/PriceService.java
@@ -0,0 +1,28 @@
+package com.tmt.ecommerce.application.services;
+
+import com.tmt.ecommerce.domain.models.PriceDto;
+import com.tmt.ecommerce.domain.ports.in.FindApplicablePriceUseCase;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PriceService {
+
+  private final FindApplicablePriceUseCase findApplicablePriceUseCase;
+
+  /**
+   * Finds the applicable price for the given product and brand at the specified application date.
+   *
+   * @param applicationDate The date when the price is applicable
+   * @param productId The ID of the product
+   * @param brandId The ID of the brand
+   * @return {@link Optional<PriceDto>} The applicable price for the given product, if not found
+   *     result, returns Optional.empty()
+   */
+  public Optional<PriceDto> findApplicablePrice(
+      LocalDateTime applicationDate, Long productId, Long brandId) {
+
+    return findApplicablePriceUseCase.findApplicablePrice(applicationDate, productId, brandId);
+  }
+}
---
+++ src/main/java/com/tmt/ecommerce/infrastructure/config/BeanPriceConfiguration.java
@@ -0,0 +1,35 @@
+package com.tmt.ecommerce.infrastructure.config;
+
+import com.tmt.ecommerce.application.services.PriceService;
+import com.tmt.ecommerce.application.usecases.FindApplicablePriceUseCaseImpl;
+import com.tmt.ecommerce.domain.ports.in.FindApplicablePriceUseCase;
+import com.tmt.ecommerce.domain.ports.out.PriceRepositoryPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanPriceConfiguration {
+
+  /**
+   * Creates a new PriceService instance.
+   *
+   * @param findApplicablePriceUseCase The use case to find the applicable price
+   * @return {@link PriceService} The price service
+   */
+  @Bean
+  public PriceService priceService(FindApplicablePriceUseCase findApplicablePriceUseCase) {
+    return new PriceService(findApplicablePriceUseCase);
+  }
+
+  /**
+   * Creates a new FindApplicablePriceUseCase instance.
+   *
+   * @param priceRepositoryPort The price repository port
+   * @return {@link FindApplicablePriceUseCase} The use case to find the applicable price
+   */
+  @Bean
+  public FindApplicablePriceUseCase findApplicablePriceUseCase(
+      PriceRepositoryPort priceRepositoryPort) {
+    return new FindApplicablePriceUseCaseImpl(priceRepositoryPort);
+  }
+}
